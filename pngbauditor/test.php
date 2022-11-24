0f24aec9e495ee5293011e7c6b8914a4ed8f773a
root <root@ip-172-31-27-40.ap-southeast-1.compute.internal>
2022-11-24 14:41:38 +0800

    firstfile

--- pngbauditor/test.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-
-	 echo $date.'<br>';
-				 echo $accountnameid.'<br>';
-				 echo $itemOption.'<br>';
-				 echo $itemType.'<br>';
-				 echo $grams.'<br>';
-?>
--- pngbsales/index3.php
+++
@@ -1,3 +0,0 @@
-
-<br><br><br>
-<center><h1>Portal is Temporarily Disabled </h2></center>
