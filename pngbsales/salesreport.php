7e6849083daf7b8c7188112fd501c14ab0a1c3cd
lekcastulo <lekcastulo@gmail.com>
2022-11-24 16:31:37 +0800

    remove total sales

--- pngbsales/salesreport.php
+++ pngbsales/salesreport.php
@@ -182,7 +182,7 @@
 
               <h3 name=totalprofit> </h3> 
 		<h3 name=totalshipping> </h3> 
-              <h3 name=overallsales> </h3> 
+              <!-- <h3 name=overallsales> </h3>  -->
               </center>
            </div>
           </div>
